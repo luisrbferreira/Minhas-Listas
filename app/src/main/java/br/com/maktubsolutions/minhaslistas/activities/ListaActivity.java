71cd867cd25ffbd0fca0a7e5d72f5390bae47de4
Luis Ferreira <luisrbferreira@gmail.com>
2016-08-22 09:34:39 -0300

    Commit

---
+++ app/src/main/java/br/com/maktubsolutions/minhaslistas/activities/ListaActivity.java
@@ -0,0 +1,13 @@
+package br.com.maktubsolutions.minhaslistas.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista);
+    }
+}
